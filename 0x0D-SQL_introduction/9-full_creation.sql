7ee2cd3ad62b6b298b8c0a17af95d3c7e6624ceb
dmbeya@must.ac.mw <dmbeya@must.ac.mw>
2022-11-12 12:40:30 +0200

    Create full table and add rows

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,12 +1,12 @@
 -- creates a table
 -- called second_table and adds multiple rows
 CREATE TABLE IF NOT EXISTS second_table(
-	id INT,
+	id INT PRIMARY KEY,,
 	name VARCHAR(256),
-	score INT,PRIMARY KEY(id));
+	score INT);
 INSERT INTO second_table(
 	id, name, score)
-VALUES (1, 'John', score = 10_);
+VALUES (1, 'John', 10);
 INSERT INTO second_table(
 	id, name, score)
 VALUES (2, 'Alex', 3);
