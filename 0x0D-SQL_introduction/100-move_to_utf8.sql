6989dc8de8cae1a0bf48123724c11c86b1899356
dmbeya@must.ac.mw <dmbeya@must.ac.mw>
2022-12-19 23:28:48 +0200

    Convert DB to unicode

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,6 @@
 -- convert database
 -- to utf
 
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE first_table CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE first_table MODIFY namee VARCHAR(255) CHARACTER SET utf8;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;;
+ALTER TABLE first_table CHANGE name VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
