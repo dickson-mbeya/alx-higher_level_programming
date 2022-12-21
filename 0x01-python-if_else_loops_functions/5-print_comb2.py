b7237ae7f1b539f1ad7080c756574914c1f36ee3
dickson-mbeya <dmbeya@must.ac.mw>
2022-12-21 11:09:24 +0200

    print numbers fro 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for ch in range(0, 100):
     if ch < 99:
         print('{:02d}'.format(ch), end =", ")
     else:
-        print('{:02d}'.format(ch), end ="\n ")
+        print('{:02d}'.format(ch), end ="\n")
