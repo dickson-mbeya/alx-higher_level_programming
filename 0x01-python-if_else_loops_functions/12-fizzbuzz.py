054dc5ac7aab17fe91147fd98086726f7dc72cd6
dickson-mbeya <dmbeya@must.ac.mw>
2024-01-24 15:21:19 +0200

    Print fizz buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("Fizz", end=" ", sep=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ", sep=" ")
+        else:
+            print(i, end=" ", sep=" ")
