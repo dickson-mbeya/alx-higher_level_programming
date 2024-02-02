8c329e8a0b00d4b2e861967565d3d7cd363994c2
dickson-mbeya <dmbeya@must.ac.mw>
2024-02-02 02:10:02 +0200

    Prints names defined by compiled  module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import hidden_4
 
-print(dir(hidden_4))
-
-if __name__ == '__main__':
-    hidden_4
+if __name__ == "__main__":
+    names = dir(hidden_4)
+    sorted_names = sorted(name for name in names if not name.startswith("__"))
+    for name in sorted_names:
+        print(name)
