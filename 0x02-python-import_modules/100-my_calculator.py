c2eb839123ba61e756076631617d960f38731f04
dickson-mbeya <dmbeya@must.ac.mw>
2024-02-02 09:31:32 +0200

    Imports functions from file calculator_1.py and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 4:
     sys.exit("Usage: ./100-my_calculator.py <a> <operator> <b>")
 
 operators = ['+', '-', '*', '/']
