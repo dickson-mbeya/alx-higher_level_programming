25b9d7c4d73ae3de763c9bbebcdfadd2741ca33f
dickson-mbeya <dmbeya@must.ac.mw>
2024-02-03 05:30:01 +0200

    Get element in a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,9 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif idx > len(my_list):
+    elif idx == 0:
+        return my_list[idx]
+    elif idx >= len(my_list):
         return None
     else:
         return my_list[idx]
