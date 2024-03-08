687810802777ed4e2ff210f4eef77d32430bc50a
dickson-mbeya <dmbeya@must.ac.mw>
2024-03-09 01:27:57 +0200

    print combination of numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i < 10:
-        print("{}{}, ".format(0,i))
-    elif i != 99:
-        print("{},".format(i))
-    else:
-        print("{}".format(i))
+for i in range(100):
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")
