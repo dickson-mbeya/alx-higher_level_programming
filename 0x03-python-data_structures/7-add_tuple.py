c51b3ce0428b935e48d1ae0bf2e706be1da45f8b
dickson-mbeya <dmbeya@must.ac.mw>
2024-03-09 22:52:31 +0200

    Add tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) < 2:
-        tuple_a = tuple_a[:2] + (0,) * (3 - len(tuple_a))
+        tuple_a = tuple_a[:2] + (0,) * (2 - len(tuple_a))
     else:
         tuple_a = tuple_a[:2]
 
     if len(tuple_b) < 2:
-        tuple_b = tuple_b[:2] + (0,) * (3 - len(tuple_b))
+        tuple_b = tuple_b[:2] + (0,) * (2 - len(tuple_b))
     else:
         tuple_b = tuple_b[:2]
 
