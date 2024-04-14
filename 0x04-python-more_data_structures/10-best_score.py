01925522b0ea9d17ea3cb9a42789b9c9b24bfcfd
dickson-mbeya <dmbeya@must.ac.mw>
2024-04-14 13:26:13 +0200

    Print maximum dictionary value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,8 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    
+
     for key, value in a_dictionary.items():
         max_value = max(a_dictionary.values())
         if value == max_value:
-            return(key)
+            return (key)
