ef748db5910a77ae93586d67e4f932395d3e4b89
dickson-mbeya <dmbeya@must.ac.mw>
2024-04-14 15:10:55 +0200

    Converts roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_int = {"I":1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 200, "M": 1000}
-    first_key, first_value = next(iter(roman_int.items()))
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 200, "M": 1000}
+    first_key, first_value = next(iter(roman.items()))
     result = 0
     prev_value = 0
 
     for char in reversed(roman_string):
-        value = roman_int[char]
+        value = roman[char]
         if value < prev_value:
             result -= value
         else:
