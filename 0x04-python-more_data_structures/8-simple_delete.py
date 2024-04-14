72d310ecf4b19a62e90897ebb8ec7818db1ab82c
dickson-mbeya <dmbeya@must.ac.mw>
2024-04-14 11:53:34 +0200

    Delete key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,6 +3,4 @@ def simple_delete(a_dictionary, key=""):
     """ deletes a key in a dictionary.f a key doesn’t exist, the dictionary won’t change"""
     if key in a_dictionary:
         del a_dictionary[key]
-        print(a_dictionary)
-    else:
-        print(a_dictionary)
+    return a_dictionary
