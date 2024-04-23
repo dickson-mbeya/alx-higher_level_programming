fd0df60606b6892ee3cdac207f174210c008d726
dickson-mbeya <dmbeya@must.ac.mw>
2024-04-23 14:04:15 +0200

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const arg = process.argv;
+if (arg.length < 3) {
+  console.log('Missing size');
+} else if (isNaN(Number(arg[2]))) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < arg[2]; i++) {
+    console.log('x'.repeat(arg[2]));
+  }
+}
