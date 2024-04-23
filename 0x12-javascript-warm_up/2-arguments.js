b33e003a540d20fa5f99f5b1dcf1d608c4dc3417
dickson-mbeya <dmbeya@must.ac.mw>
2024-04-23 11:15:00 +0200

    check arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+const args = process.argv;
+const numArgs = args.length - 2;
+
+if (numArgs < 1) {
+  console.log('No argument');
+} else if (numArgs === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
