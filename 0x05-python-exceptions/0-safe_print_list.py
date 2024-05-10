1275466d2f679bf024bfdaf2f8f75dea18925814
dickson-mbeya <dmbeya@must.ac.mw>
2024-05-10 12:43:59 +0300

    Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,7 @@
+try:
+    def safe_print_list(my_list=[], x=0):
+        new_list = my_list[:x]
+        result = int(''.join(map(str, new_list)))
+        return result
+except IndexError:
+    pass
