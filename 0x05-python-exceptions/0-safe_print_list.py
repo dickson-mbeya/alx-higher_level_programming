7fa474abc3fd0a4362bb233ffe6f083ce381dcfc
dickson-mbeya <dmbeya@must.ac.mw>
2024-05-10 13:03:32 +0300

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@ try:
     def safe_print_list(my_list=[], x=0):
         new_list = my_list[:x]
         result = int(''.join(map(str, new_list)))
-        print(x)
-        return result
+        print(result)
+        return x
 except IndexError:
     pass
 except ValueError:
